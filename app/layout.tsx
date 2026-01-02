5d18de38ac07e50b313cb001a932101b9d4ac15c
Adityaa Mehra <adirakash@gmail.com>
2026-01-02 12:52:05 +0530

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,22 +30,8 @@ export const metadata = {
     images: ["https://metatags.io/images/meta-tags.png"],
   },
 
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+   icons: {
+    shortcut: "/favicon.png",
   },
 };
 
